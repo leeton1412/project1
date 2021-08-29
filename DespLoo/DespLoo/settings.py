25c2c954f917ed7d1364672028738e70362d38e5
leeton1412 <leighton.griffith87@gmail.com>
2021-08-29 13:35:05 +0000

    Settled on background image

--- DespLoo/DespLoo/settings.py
+++ DespLoo/DespLoo/settings.py
@@ -117,8 +117,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+# Dont Forget These
+
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),) # Important
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
